fa67001bebd95975c25dacdcdf2fe9430fdcbdce
Dev-Emmyy <emmanuelugochukwu2000@gmail.com>
2024-07-27 16:42:16 +0100

    commit

--- src/app/components/VideoBackground.jsx
+++ src/app/components/VideoBackground.jsx
@@ -1,16 +1,9 @@
 "use client"
-import { React,useEffect, useRef } from 'react';
+import { React} from 'react';
 
 
 const VideoBackground = () => {
 
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.playbackRate = 0.9; // Adjust the playback rate as needed
-    }
-  }, []);
 
   return (
     <div className="absolute inset-0 w-full h-full overflow-hidden rounded-2xl">
@@ -19,7 +12,6 @@ const VideoBackground = () => {
         autoPlay 
         muted 
         loop 
-        ref={videoRef}
         className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto object-cover transform -translate-x-1/2 -translate-y-1/2 filter brightness-75"
       >
         <source src="/video/background.mp4" type="video/mp4" />
