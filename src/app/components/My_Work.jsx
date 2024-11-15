a78918010a1ac801864ca49d8d98872ed177509f
Emmanuel <emmanuelugochukwu2000@gmail.com>
2024-11-15 17:35:26 +0100

    commit

--- src/app/components/My_Work.jsx
+++ src/app/components/My_Work.jsx
@@ -6,12 +6,12 @@ const My_Work = () => {
     {
       image: "/work/movieflix.jpg",
       title: "Movie Web App",
-      link: "https://movie-flix-master.vercel.app/"
+      link: "https://movie-data-base-lyart.vercel.app/"
     },
     {
       image: "/work/superhero.jpg",
       title: "Superhero App",
-      link: "https://superhero-app-link.com"
+      link: "https://superhero-app-chi.vercel.app/"
     },
     {
       image: "/work/photo_gallery.jpg",
@@ -36,7 +36,7 @@ const My_Work = () => {
   ];
 
   return (
-    <div className="container mx-auto px-4 py-10">
+     <div className="container mx-auto px-4 py-10">
       <h1 className='text-4xl font-bold mb-10 text-white text-center font-mono'>My<span className='font-sans'>Work</span></h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {allProjects.map((project, index) => (
